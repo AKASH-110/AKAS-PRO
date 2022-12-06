fa19e444c998732ab103a8d635cfb8d52d9356b9
HADI ANHAF AIMAN <117533758+AKASH-110@users.noreply.github.com>
2022-12-06 23:07:28 +0600

    Create AKASH.py

---
+++ AKASH.py
@@ -0,0 +1,23 @@
+import os, platform, time
+
+try:
+
+    import requests
+
+except:
+
+    os.system('pip install requests')
+
+os.system('git pull')
+
+import requests
+
+bit = platform.architecture()[0]
+
+if bit == '64bit':
+
+    import AK
+
+elif bit == '32bit':
+
+    import AK
